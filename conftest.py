de3471405404d0b535e71ba970bc12633ee59e28
Stepan Sergienko <stephan.sergienko@gmail.com>
2023-08-19 08:09:52 +0300

    corrected the conftest file

--- conftest.py
+++ conftest.py
@@ -1,18 +1,27 @@
+import os
 
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import browser
+from selene import Browser, Config
+from dotenv import load_dotenv
 
 from utils import attach
 
 DEFAULT_BROWSER_VERSION = "100.0"
+
 
 def pytest_addoption(parser):
     parser.addoption(
         '--browser_version',
         default='100.0'
     )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
@@ -29,12 +38,14 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
 
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser.config.driver = driver
+    browser = Browser(Config(driver))
 
     yield browser
 
