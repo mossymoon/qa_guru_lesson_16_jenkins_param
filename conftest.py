06e5889b5f5393cac63a2e4a44dbb0a71e41c7d3
Stepan Sergienko <stephan.sergienko@gmail.com>
2023-08-19 10:40:22 +0300

    added env file

--- conftest.py
+++ conftest.py
@@ -39,7 +39,7 @@ def browser_management(request):
 
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     browser.config.driver = driver
