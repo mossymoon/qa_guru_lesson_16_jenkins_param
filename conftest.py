b9b18db986958701ce9dc5dde1129e7d7d57d57e
Stepan Sergienko <stephan.sergienko@gmail.com>
2023-08-19 08:38:04 +0300

    corrected the conftest file2

--- conftest.py
+++ conftest.py
@@ -1,9 +1,9 @@
 import os
 
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+import pytest
+from selene import browser
 from dotenv import load_dotenv
 
 from utils import attach
@@ -23,8 +23,8 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope='function')
-def setup_browser(request):
+@pytest.fixture(scope='function', autouse=True)
+def browser_management(request):
     browser_version = request.config.getoption('--browser_version')
     browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
@@ -36,16 +36,19 @@ def setup_browser(request):
             "enableVideo": True
         }
     }
-    options.capabilities.update(selenoid_capabilities)
-
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
 
+    options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser = Browser(Config(driver))
+    browser.config.driver = driver
+
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     yield browser
 
@@ -53,4 +56,5 @@ def setup_browser(request):
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_video(browser)
+
     browser.quit()
